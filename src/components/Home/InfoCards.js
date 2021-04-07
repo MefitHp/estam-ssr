ae901fc4804debd3821a31b8b479932cd326c7b0
MefitHp <mefitdev@gmail.com>
2021-04-06 21:33:44 -0500

    Swiched card display property from flex to grid

--- src/components/Home/InfoCards.js
+++ src/components/Home/InfoCards.js
@@ -5,31 +5,36 @@ import { Wrapper } from "../shared";
 
 const cards = [
   {
-    logo: "",
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/IMDb_Logo_Square.svg/1200px-IMDb_Logo_Square.svg.png",
     title: "Generación / Almacenamiento",
     description:
       "Tenemos 5 esquemas diferentesde generar energía en sitio y almacenarla para usarla cuandolas tarifas son más altas.",
   },
   {
-    logo: "",
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/IMDb_Logo_Square.svg/1200px-IMDb_Logo_Square.svg.png",
     title: "Código de Red",
     description:
       "Derivado de las problemáticas actuales del sistema eléctrico nacional se decretan las condiciones que debe de cumplirvtodo usuario conectado al sen, llamadas código de red.",
   },
   {
-    logo: "",
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/IMDb_Logo_Square.svg/1200px-IMDb_Logo_Square.svg.png",
     title: "Software de Administración de Energía",
     description:
       "Monitoreo de líneas de producción y consumos en tiempo real a través de un software en la nube",
   },
   {
-    logo: "",
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/IMDb_Logo_Square.svg/1200px-IMDb_Logo_Square.svg.png",
     title: "SGEn",
     description:
       "Basado en la ISO50001 se elabora un sistema de gestión de energía con auditores expertos",
   },
   {
-    logo: "",
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/IMDb_Logo_Square.svg/1200px-IMDb_Logo_Square.svg.png",
     title: "MEM",
     description:
       "Al ser comercializadores de energía te ayudamos en tu trámite como usuario calificado con una excelente oferta de suministro.",
@@ -43,14 +48,13 @@ const InfoCards = () => {
         <CardsContainer>
           {cards.map(({ logo, title, description }, key) => (
             <Card key={key}>
-              <div className="card card-logo">
-                {/* <img src={logo} /> */}
-                <div className="logo"></div>
+              <div className="card-logo flex-alignment">
+                <img src={logo} alt={title} />
               </div>
-              <div className="card card-title">
-                <h4>{title}</h4>
+              <div className="card-title flex-alignment">
+                <h5>{title}</h5>
               </div>
-              <div className="card card-text">{description}</div>
+              <div className="card-text">{description}</div>
             </Card>
           ))}
         </CardsContainer>
@@ -61,29 +65,33 @@ const InfoCards = () => {
 
 const CardsSection = styled.section`
   background-color: #fff;
+  padding: 2em;
 `;
 
 const CardsContainer = styled.section`
-  height: 80vh;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  gap: 8px;
 `;
 
 const Card = styled.div`
-  width: 200px;
-  height: 350px;
-  padding: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.33);
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  gap: 8px;
   border-radius: 5px;
-  .card {
+  height: 100%;
+  .flex-alignment {
     display: flex;
-    justify-content: center;
     align-items: center;
+    justify-content: center;
   }
   .card-logo {
-    height: 25%;
+    height: 100px;
     img {
-      height: 100%;
+      height: 70px;
+      width: 70px;
     }
     .logo {
       width: 50px;
@@ -97,16 +105,14 @@ const Card = styled.div`
     }
   }
   .card-title {
-    height: 35%;
-    font-size: 1.6rem;
+    height: 40px;
     text-align: center;
     line-height: normal;
     font-weight: 600;
   }
   .card-text {
     height: 40%;
-    font-size: 0.7rem;
-    text-align: justify;
+    font-size: 0.8rem;
     align-items: flex-start;
   }
 `;
