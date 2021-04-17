0ad5e41fa193d911d438b9a6e7a1e6521d770828
Mefit Hernández <mefitdev@gmail.com>
2021-04-17 14:57:12 -0500

    Working on navbar responsive

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import tw from "twin.macro";
 import { Link } from "gatsby";
@@ -6,6 +6,104 @@ import WhiteLogo from "../../images/white_logo.png";
 
 //Local Imports
 import Button from "./Button";
+
+const items = [
+  {
+    label: "Noticias",
+    path: "/noticias",
+  },
+  {
+    label: "Administración de Energía",
+    path: "/administracion-energia",
+  },
+  {
+    label: "Nosotros",
+    path: "/nosotros",
+  },
+];
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <Logo src={WhiteLogo} alt="ESTAM LOGO" />
+      </Link>
+      {/* Hidden in mobile */}
+      <LinkList>
+        {items.map(({ label, path }, index) => {
+          return (
+            <ListItem key={index}>
+              <MenuLink key={index} to={path}>
+                {label}
+              </MenuLink>
+            </ListItem>
+          );
+        })}
+      </LinkList>
+      <ListItem className="contact-button">
+        <Button>
+          Contacto <span style={{ fontSize: 12, paddingLeft: 6 }}> ▶</span>
+        </Button>
+      </ListItem>
+      {/* Hidden in mobile */}
+      {/* Shown in mobile */}
+      <MenuButton
+        style={{ color: "white", cursor: "pointer" }}
+        onClick={() => setMenuOpen((prev) => !prev)}
+      >
+        {menuOpen ? "Cerrar" : "Abrir"}
+      </MenuButton>
+      {/* Shown in mobile */}
+      <ResponsiveLinkList isOpen={menuOpen}>
+        {items.map(({ label, path }, index) => {
+          return (
+            <ListItem key={index}>
+              <MenuLink key={index} to={path}>
+                {label}
+              </MenuLink>
+            </ListItem>
+          );
+        })}
+      </ResponsiveLinkList>
+    </HeaderContainer>
+  );
+};
+
+const MenuButton = styled.button`
+  display: none;
+  font-size: 1rem;
+  padding: 4px;
+  color: white;
+  border: 1px solid white;
+  width: 60px;
+  height: 60px;
+  @media (max-width: 700px) {
+    display: block;
+  }
+`;
+
+const ResponsiveLinkList = styled.div`
+  background-color: black;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  overflow: hidden;
+  height: ${({ isOpen }) => (isOpen ? "100%" : "0px")};
+  position: absolute;
+  top: 80px;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  transition: height 200ms ease-out;
+
+  display: none;
+  @media (max-width: 700px) {
+    display: flex;
+  }
+`;
 
 const HeaderContainer = styled.nav`
   ${tw`
@@ -18,6 +116,7 @@ const HeaderContainer = styled.nav`
   justify-between
   lg:px-20
   `}
+  position: relative;
   color: black;
   .contact-button {
     width: 130px;
@@ -66,46 +165,4 @@ const MenuLink = styled(Link)`
     border-color: white;
   }
 `;
-
-const items = [
-  {
-    label: "Noticias",
-    path: "/noticias",
-  },
-  {
-    label: "Administración de Energía",
-    path: "/administracion-energia",
-  },
-  {
-    label: "Nosotros",
-    path: "/nosotros",
-  },
-];
-
-const Navbar = () => {
-  return (
-    <HeaderContainer>
-      <Link to="/">
-        <Logo src={WhiteLogo} alt="ESTAM LOGO" />
-      </Link>
-      <LinkList>
-        {items.map(({ label, path }, index) => {
-          return (
-            <ListItem key={index}>
-              <MenuLink key={index} to={path}>
-                {label}
-              </MenuLink>
-            </ListItem>
-          );
-        })}
-      </LinkList>
-      <ListItem className="contact-button">
-        <Button>
-          Contacto <span style={{ fontSize: 12, paddingLeft: 6 }}> ▶</span>
-        </Button>
-      </ListItem>
-    </HeaderContainer>
-  );
-};
-
 export default Navbar;
