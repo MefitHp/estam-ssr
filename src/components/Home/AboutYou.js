08d47f7f79b0bc70143f7f6871058de5f97e178e
Mefit Hernández <mefitdev@gmail.com>
2021-04-21 18:33:38 -0500

    Footer and adjusments in form

--- src/components/Home/AboutYou.js
+++ src/components/Home/AboutYou.js
@@ -17,29 +17,26 @@ const AboutYouContainer = styled.section`
       font-size: 42px;
     }
   }
-
+  .send {
+    width: 70px;
+    color: var(--blue);
+  }
   form {
-    display: grid;
     width: 100%;
-    .name {
-      grid-area: name;
-    }
-    .email {
-      grid-area: email;
-    }
-    .message {
-      grid-area: message;
-    }
-    .send {
-      grid-area: send;
-      color: var(--blue);
-    }
     gap: 8px;
-    grid-template-areas:
-      "name name email email"
-      "message message message send";
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    .column {
+      display: flex;
+      flex-direction: column;
+      flex: 1 1 48%;
+    }
+
     input,
-    textarea {
+    textarea,
+    select {
       border-radius: 4px;
       padding: 10px;
       background-color: #f4f4f4;
@@ -59,12 +56,20 @@ const AboutYouContainer = styled.section`
   @media (max-width: 700px) {
     grid-template-columns: 1fr;
     padding: 2rem;
+    .column {
+      display: flex;
+      flex-direction: column;
+      width: inherit;
+      flex: 1;
+    }
   }
 `;
 
 const FormContainer = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  gap: 12px;
+  justify-content: flex-end;
 `;
 const AboutYou = () => {
   return (
@@ -77,18 +82,99 @@ const AboutYou = () => {
           </h2>
         </aside>
         <FormContainer>
-          <form>
-            <input type="text" className="name" placeholder="Nombre:" />
-            <input type="email" className="email" placeholder="Email: " />
-            <textarea className="message" placeholder="Mensaje: " />
-            <button className="send" type="submit">
-              Enviar <span style={{ fontSize: 12, paddingLeft: 6 }}> ▶</span>
-            </button>
+          <form id="contact-us">
+            <input
+              type="text"
+              className="column"
+              placeholder="Nombre:"
+              required
+            />
+            <input
+              type="email"
+              className="column"
+              placeholder="Email: "
+              required
+            />
+            <input
+              type="company"
+              className="column"
+              placeholder="Empresa: "
+              required
+            />
+            <input
+              type="jobTitle"
+              className="column"
+              placeholder="Puesto: "
+              required
+            />
+            <select
+              name="Location"
+              className="column"
+              placeholder="Ubicación: "
+              required
+            >
+              <StateOptions />
+            </select>
+            <input
+              type="interests"
+              className="column"
+              placeholder="¿En que estas interesado? "
+              required
+            />
+            <input
+              style={{ width: "98%" }}
+              type="averageConsumption"
+              className="column"
+              placeholder="Promedio de pago de energía mensual "
+              required
+            />
           </form>
+          <button className="send" type="submit" form="contact-us">
+            Enviar <span style={{ fontSize: 12, paddingLeft: 6 }}> ▶</span>
+          </button>
         </FormContainer>
       </AboutYouContainer>
     </Wrapper>
   );
 };
 
+const StateOptions = () => {
+  return (
+    <>
+      <option value="no">Seleccione uno...</option>
+      <option value="Aguascalientes">Aguascalientes</option>
+      <option value="Baja California">Baja California</option>
+      <option value="Baja California Sur">Baja California Sur</option>
+      <option value="Campeche">Campeche</option>
+      <option value="Chiapas">Chiapas</option>
+      <option value="Chihuahua">Chihuahua</option>
+      <option value="CDMX">Ciudad de México</option>
+      <option value="Coahuila">Coahuila</option>
+      <option value="Colima">Colima</option>
+      <option value="Durango">Durango</option>
+      <option value="Estado de México">Estado de México</option>
+      <option value="Guanajuato">Guanajuato</option>
+      <option value="Guerrero">Guerrero</option>
+      <option value="Hidalgo">Hidalgo</option>
+      <option value="Jalisco">Jalisco</option>
+      <option value="Michoacán">Michoacán</option>
+      <option value="Morelos">Morelos</option>
+      <option value="Nayarit">Nayarit</option>
+      <option value="Nuevo León">Nuevo León</option>
+      <option value="Oaxaca">Oaxaca</option>
+      <option value="Puebla">Puebla</option>
+      <option value="Querétaro">Querétaro</option>
+      <option value="Quintana Roo">Quintana Roo</option>
+      <option value="San Luis Potosí">San Luis Potosí</option>
+      <option value="Sinaloa">Sinaloa</option>
+      <option value="Sonora">Sonora</option>
+      <option value="Tabasco">Tabasco</option>
+      <option value="Tamaulipas">Tamaulipas</option>
+      <option value="Tlaxcala">Tlaxcala</option>
+      <option value="Veracruz">Veracruz</option>
+      <option value="Yucatán">Yucatán</option>
+      <option value="Zacatecas">Zacatecas</option>
+    </>
+  );
+};
 export default AboutYou;
--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 
+import image from "../../images/PROYECTO1.png";
 import { Wrapper } from "../shared";
 const image1 = "/static/PORQUE.png";
 const image2 = "/static/COMOFUNCIONAMOS.png";
@@ -14,26 +15,26 @@ const Projects = () => {
           <div className="column one">
             <Text>Generación y Almacenamiento</Text>
             <div>
-              <Image src={image1} alt={"title"} />
+              <Image src={image} alt={"title"} />
             </div>
           </div>
           <div className="column two">
             <Text>Generación y Almacenamiento</Text>
             <div>
-              <Image src={image1} alt={"title"} />
+              <Image src={image} alt={"title"} />
             </div>
             <div className="flex-images">
-              <Image src={image1} alt={"title"} />
-              <Image src={image1} alt={"title"} />
+              <Image src={image} alt={"title"} />
+              <Image src={image} alt={"title"} />
             </div>
           </div>
           <div className="column three">
             <Text>Generación y Almacenamiento</Text>
             <div>
-              <Image src={image1} alt={"title"} />
+              <Image src={image} alt={"title"} />
             </div>
             <div>
-              <Image src={image1} alt={"title"} />
+              <Image src={image} alt={"title"} />
             </div>
           </div>
         </Container>
