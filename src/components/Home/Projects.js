2b33336068aade75a60618cf4fa9dc5d555ede76
Oswaldo Martinez <oswalfut_96@hotmail.com>
2021-04-27 22:24:50 -0500

    added Fullscreen picture

--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 import { Wrapper } from "../shared";
@@ -10,6 +10,11 @@ import image5 from "../../images/PROYECTO5.png";
 import image6 from "../../images/PROYECTO6.png";
 
 const Projects = () => {
+  const [currentPicture, setCurrentPicture] = useState(null);
+  const closeModal = () => {
+    setCurrentPicture(null);
+  };
+
   return (
     <Section>
       <Wrapper>
@@ -18,37 +23,88 @@ const Projects = () => {
           <div className="column one">
             <Text>Generación y Almacenamiento</Text>
             <ImageContainer>
-              <Image src={image1} alt={"Paneles Vista Superior"} />
+              <Image
+                src={image1}
+                alt={"Paneles Vista Superior"}
+                onClick={() => setCurrentPicture(image1)}
+              />
             </ImageContainer>
           </div>
           <div className="column two">
             <Text>Código de Red</Text>
             <ImageContainer>
-              <Image src={image2} alt={"Electricidad"} />
+              <Image
+                src={image2}
+                alt={"Electricidad"}
+                onClick={() => setCurrentPicture(image2)}
+              />
             </ImageContainer>
             <div className="flex-images">
               <ImageContainer>
-                <Image src={image3} alt={"Electricidad 2"} />
+                <Image
+                  src={image3}
+                  alt={"Electricidad 2"}
+                  onClick={() => setCurrentPicture(image3)}
+                />
               </ImageContainer>
               <ImageContainer>
-                <Image src={image4} alt={"Electricidad 3"} />
+                <Image
+                  src={image4}
+                  alt={"Electricidad 3"}
+                  onClick={() => setCurrentPicture(image4)}
+                />
               </ImageContainer>
             </div>
           </div>
           <div className="column three">
             <Text>MEM</Text>
             <ImageContainer>
-              <Image src={image5} alt={"Panel Solar"} />
+              <Image
+                src={image5}
+                alt={"Panel Solar"}
+                onClick={() => setCurrentPicture(image5)}
+              />
             </ImageContainer>
             <ImageContainer>
-              <Image src={image6} alt={"Panel Solar 2"} />
+              <Image
+                src={image6}
+                alt={"Panel Solar 2"}
+                onClick={() => setCurrentPicture(image6)}
+              />
             </ImageContainer>
           </div>
         </Container>
       </Wrapper>
+      {currentPicture && (
+        <Overlay>
+          <CloseIcon onClick={closeModal}>X</CloseIcon>
+          <BigImage src={currentPicture} alt={"CurrentPicture"} />
+        </Overlay>
+      )}
     </Section>
   );
 };
+
+const CloseIcon = styled.span`
+  position: fixed;
+  top: 10px;
+  right: 10px;
+  z-index: 10;
+  cursor: pointer;
+`;
+
+const Overlay = styled.div`
+  padding: 20px;
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.9);
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+`;
 
 const ImageContainer = styled.div`
   margin-right: 10px;
@@ -61,6 +117,20 @@ const ImageContainer = styled.div`
 const Image = styled.img`
   height: 100%;
   width: 100%;
+  cursor: pointer;
+`;
+
+const BigImage = styled.div`
+  height: 80%;
+  width: 80%;
+  background-image: url(${({ src }) => src});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  @media (max-width: 700px) {
+    height: 100%;
+    width: 100%;
+  }
 `;
 
 const Text = styled.h3`
@@ -74,6 +144,7 @@ const Title = styled.h2`
 `;
 
 const Section = styled.section`
+  position: relative;
   padding: 60px 0px;
   background: #000;
   color: #fff;
